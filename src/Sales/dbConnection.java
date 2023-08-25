30ab451ec0a0af969843a0871d573f48f6df4814
Dilravi Dhananja <107805922+dhananja6557@users.noreply.github.com>
2023-08-25 08:59:26 +0530

    Level_1

--- src/Sales/dbConnection.java
+++ src/Sales/dbConnection.java
@@ -8,6 +8,41 @@ package Sales;
  *
  * @author eSolution
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author eSolution
+ */
 public class dbConnection {
-    
+    public void db_con(String  query, String data) {
+        String url = "jdbc:mysql://localhost/esolution_pos";
+        String uname = "root";
+        String pword = "test@123";
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url, uname, pword);
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            rs.next();
+            String datas = rs.getString(data);
+            
+            System.out.println(datas);
+            
+            st.close();
+            con.close();
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(dbConnection.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(dbConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
