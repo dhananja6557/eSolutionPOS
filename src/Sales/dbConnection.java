2ce257a5898343d980e5062717923ff140083ab1
Dilravi Dhananja <107805922+dhananja6557@users.noreply.github.com>
2023-09-03 10:37:10 +0530

    Level_5

--- src/Sales/dbConnection.java
+++ src/Sales/dbConnection.java
@@ -21,48 +21,21 @@ import java.util.logging.Logger;
  * @author eSolution
  */
 public class dbConnection {
-//    public void db_con(String  query, String data) {
-//        String url = "jdbc:mysql://localhost/esolution_pos";
-//        String uname = "root";
-//        String pword = "test@123"; 
-//        
-//        try {
-//            Class.forName("com.mysql.jdbc.Driver");
-//            Connection con = DriverManager.getConnection(url, uname, pword);
-//            Statement st = con.createStatement();
-//            ResultSet nrs = st.executeQuery(query);
-////            rs.next();
-//            while(nrs.next()) {
-//                String datas = nrs.getString(data);
-//                System.out.println(datas);
-//            }
-////            String datas = rs.getString(data);
-//            
-//            st.close();
-//            con.close();
-//            
-//        } catch (ClassNotFoundException ex) {
-//            Logger.getLogger(dbConnection.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (SQLException ex) {
-//            Logger.getLogger(dbConnection.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
-    
-    static {
+    public void db_con(String  query, String data) {
         String url = "jdbc:mysql://localhost/esolution_pos";
         String uname = "root";
-        String pword = "test@123";         
+        String pword = "test@123"; 
         
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(url, uname, pword);
             Statement st = con.createStatement();
-            ResultSet nrs = st.executeQuery(" SELECT * FROM es_product");
+            ResultSet nrs = st.executeQuery(query);
 //            rs.next();
-//            while(nrs.next()) {
-//                String datas = nrs.getString(data);
-//                System.out.println(datas);
-//            }
+            while(nrs.next()) {
+                String datas = nrs.getString(data);
+                System.out.println(datas);
+            }
 //            String datas = rs.getString(data);
             
             st.close();
@@ -74,12 +47,39 @@ public class dbConnection {
             Logger.getLogger(dbConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+//    static {
+//        String url = "jdbc:mysql://localhost/esolution_pos";
+//        String uname = "root";
+//        String pword = "test@123";         
+//        
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Connection con = DriverManager.getConnection(url, uname, pword);
+//            Statement st = con.createStatement();
+//            ResultSet nrs = st.executeQuery(" SELECT * FROM es_product");
+////            rs.next();
+////            while(nrs.next()) {
+////                String datas = nrs.getString(data);
+////                System.out.println(datas);
+////            }
+////            String datas = rs.getString(data);
+//            
+//            st.close();
+//            con.close();
+//            
+//        } catch (ClassNotFoundException ex) {
+//            Logger.getLogger(dbConnection.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (SQLException ex) {
+//            Logger.getLogger(dbConnection.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
         
     
     public static void main(String[] args) {
         dbConnection dbcon = new dbConnection();
         String db = "SELECT * FROM es_product";
-//        dbcon.db_con(db, "product_name");
+        dbcon.db_con(db, "product_name");
 //        System.out.println("1");
     }
 }
